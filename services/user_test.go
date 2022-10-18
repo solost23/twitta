8e7f774553436e2e32c87203e3ee38c97ecb3dfc
ty <2805347171@qq.com>
2022-10-18 10:10:33 +0800

    feat: add user test

---
+++ services/user_test.go
@@ -0,0 +1,65 @@
+package services
+
+import (
+	"Twitta/forms"
+	"Twitta/pkg/utils"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/http"
+	"testing"
+)
+
+func TestService_UserSearch(t *testing.T) {
+	ginCtx, _ := gin.CreateTestContext(&http.TestResponseWriter{})
+	type arg struct {
+		ctx    *gin.Context
+		params *forms.SearchForm
+	}
+	type want struct {
+		results []*forms.UserDetailResponse
+		err     error
+	}
+	tests := []struct {
+		arg  arg
+		want want
+	}{
+		{
+			arg: arg{
+				ctx: ginCtx,
+				params: &forms.SearchForm{
+					PageForm: utils.PageForm{
+						Page: 1,
+						Size: 10,
+					},
+					Keyword: "user",
+				},
+			},
+			want: want{
+				err: nil,
+			},
+		},
+		{
+			arg: arg{
+				ctx: ginCtx,
+				params: &forms.SearchForm{
+					PageForm: utils.PageForm{
+						Page: 1,
+						Size: 10,
+					},
+					Keyword: "用户",
+				},
+			},
+			want: want{
+				err: nil,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		results, err := (&Service{}).UserSearch(test.arg.ctx, test.arg.params)
+		if err != test.want.err {
+			t.Errorf("%v \n", err.Error())
+		}
+		fmt.Printf("results: %v \n", results)
+	}
+}
